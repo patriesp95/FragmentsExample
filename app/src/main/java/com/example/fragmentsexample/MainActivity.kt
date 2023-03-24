a220308fa06352a186d0654372572490a048cc80
patriesp95 <patrimespert@outlook.com>
2023-03-24 12:28:10 +0100

    initial commit

--- app/src/main/java/com/example/fragmentsexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsexample/MainActivity.kt
@@ -6,26 +6,11 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fragmentsexample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),OnFragmentActionsListener{
-
+class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.btnBlue.setOnClickListener{replaceFragment(BlueFragment())}
-        binding.btnRed.setOnClickListener{replaceFragment(RedFragment())}
-    }
-
-    override fun onClickFragmentButton() {
-        Toast.makeText(this, "El botón ha sido pulsado", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/example/fragmentsexample/OnFragmentActionsListener.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fragmentsexample
-
-interface OnFragmentActionsListener {
-    fun onClickFragmentButton()
-}
