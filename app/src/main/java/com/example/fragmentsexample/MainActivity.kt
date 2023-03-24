0fb5abceac79672d47758cfa9672b8dd55893b2f
patriesp95 <patrimespert@outlook.com>
2023-03-24 11:41:04 +0100

    example 2 vertical

--- app/src/main/java/com/example/fragmentsexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsexample/MainActivity.kt
@@ -2,25 +2,10 @@ package com.example.fragmentsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.os.bundleOf
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import com.example.fragmentsexample.FirstFragment.Companion.ADDRESS_BUNDLE
-import com.example.fragmentsexample.FirstFragment.Companion.NAME_BUNDLE
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if(savedInstanceState == null){
-            val bundle = bundleOf(NAME_BUNDLE to "Patri",
-                ADDRESS_BUNDLE to "casa 123")
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<FirstFragment>(R.id.fragmentContainer, args = bundle)
-            }
-        }
-
     }
 }
