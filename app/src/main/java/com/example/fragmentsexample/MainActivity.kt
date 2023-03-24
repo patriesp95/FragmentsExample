41dbfd3f3d6bd5d3a420765097d5e0c4cce630ed
patriesp95 <patrimespert@outlook.com>
2023-03-24 12:16:04 +0100

    loading fragments when clicking a button

--- app/src/main/java/com/example/fragmentsexample/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.fragmentsexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.fragmentsexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(),OnFragmentActionsListener{
@@ -12,9 +13,18 @@ class MainActivity : AppCompatActivity(),OnFragmentActionsListener{
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnBlue.setOnClickListener{loadFragment(BlueFragment())}
+        binding.btnRed.setOnClickListener{loadFragment(RedFragment())}
     }
 
     override fun onClickFragmentButton() {
         Toast.makeText(this, "El botón ha sido pulsado", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 }
